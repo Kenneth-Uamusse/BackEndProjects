5a42d6f46fb074228434c2ff8197c12fd28bf9fe
Kenneth Uamusse <kennethuamusse10@gmail.com>
2025-02-06 11:43:30 +0200

    User registration feature added

---
+++ Vacancy-Managment/src/controllers/AuthController.ts
@@ -0,0 +1,15 @@
+import { HttpError } from "../errors/HttpError";
+import { UsersModel } from "../models/UsersModel";
+import { UserRegistrationSchema } from "../schema/allSchemas";
+import { Request, Response } from "express";
+
+export class AuthController {
+  register = (req: Request, res: Response) => {
+    const user = UserRegistrationSchema.parse(req.body);
+
+    const newUser = UsersModel.userRegistration(user);
+    if (!newUser) throw new HttpError(400, "Email already in use!!");
+
+    res.status(201).json({ newUser });
+  };
+}
