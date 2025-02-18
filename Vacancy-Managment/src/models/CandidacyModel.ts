2fd903d1bb07d8dfe9caf853136c23c484a6a496
Kenneth Uamusse <kennethuamusse10@gmail.com>
2025-02-18 14:35:52 +0200

    Candidacy model implemented v1.0.0

---
+++ Vacancy-Managment/src/models/CandidacyModel.ts
@@ -0,0 +1,41 @@
+import CandidacyAttributes from "../entities/candidacyInterface";
+
+export class CandidacyModel {
+  id: number;
+  candidateName: string;
+  candidateEmail: string;
+  candidatePhone: number;
+  status: "pending" | "approved" | "rejected";
+
+  private static candidacies: CandidacyModel[] = [];
+  private static nextId: number = CandidacyModel.calculateNextId();
+
+  constructor(attributes: CandidacyAttributes) {
+    this.id = attributes.id;
+    this.candidateName = attributes.candidateName;
+    this.candidateEmail = attributes.candidateEmail;
+    this.candidatePhone = attributes.candidatePhone;
+    this.status = attributes.status;
+  }
+
+  private static calculateNextId(): number {
+    const maxId = this.candidacies.reduce(
+      (max, candidacy) => (candidacy.id > max ? candidacy.id : max),
+      0
+    );
+    return maxId + 1;
+  }
+
+  static applyForJob(
+    attributes: Omit<CandidacyAttributes, "id" | "status">
+  ): CandidacyModel {
+    const newId = this.nextId++;
+    const candidacy = new CandidacyModel({
+      id: newId,
+      status: "pending",
+      ...attributes,
+    });
+
+    return candidacy;
+  }
+}
