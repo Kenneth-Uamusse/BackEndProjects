7a9aad56e211ac8761d14b8a5d41773d6201f4a6
Kenneth Uamusse <kennethuamusse10@gmail.com>
2025-02-19 13:35:34 +0200

    Get candidacy by id route implementation

--- Vacancy-Managment/src/routes/candidacyRoutes.ts
+++ Vacancy-Managment/src/routes/candidacyRoutes.ts
@@ -4,6 +4,8 @@ import { CandidacyController } from "../controllers/CandidacyController";
 const candidacyRouter = express.Router();
 const candidacyController = new CandidacyController();
 
+
+candidacyRouter.get('/candidacies/:id', candidacyController.show)
 candidacyRouter.post("/applies/:id", candidacyController.apply);
 
 export default candidacyRouter
