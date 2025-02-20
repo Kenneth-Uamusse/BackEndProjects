ff7e86b4eb2bc68a9cd82347ab0fdc78f05c29a4
Kenneth Uamusse <kennethuamusse10@gmail.com>
2025-02-20 11:01:20 +0200

    Ensure user is auth and admin for candidacy routes functionality implementation

--- Vacancy-Managment/src/routes/candidacyRoutes.ts
+++ Vacancy-Managment/src/routes/candidacyRoutes.ts
@@ -1,12 +1,31 @@
 import express from "express";
 import { CandidacyController } from "../controllers/CandidacyController";
+import { AuthMiddleware } from "../middlewares/AuthMiddleware";
 
 const candidacyRouter = express.Router();
 const candidacyController = new CandidacyController();
+const authMiddleware = new AuthMiddleware();
 
-candidacyRouter.get("/candidacies", candidacyController.index);
-candidacyRouter.get("/candidacies/:id", candidacyController.show);
+candidacyRouter.use(authMiddleware.ensureUserIsAuth);
+
+candidacyRouter.get(
+  "/candidacies",
+  authMiddleware.ensureUserIsAdmin,
+  candidacyController.index
+);
+
+candidacyRouter.get(
+  "/candidacies/:id",
+  authMiddleware.ensureUserIsAdmin,
+  candidacyController.show
+);
+
 candidacyRouter.post("/applies/:id", candidacyController.apply);
-candidacyRouter.post("/candidacy/changeStatus/:id", candidacyController.change);
+
+candidacyRouter.post(
+  "/candidacy/changeStatus/:id",
+  authMiddleware.ensureUserIsAdmin,
+  candidacyController.change
+);
 
 export default candidacyRouter;
